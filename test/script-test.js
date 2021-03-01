4aa329aee9741690721fc64c43a49d346ad1cf69
MASDX <sirawitt42@gmail.com>
2021-03-01 03:58:06 -0500

    refactor uint-test

--- test/script-test.js
+++ test/script-test.js
@@ -7,10 +7,9 @@ describe("Non Fungible Token (NFT)", function() {
   const zero_address = 0x0000000000000000000000000000000000000000;
   const attribute = {
     name: "TEST_NFT",
-    symbol: "TNFT",
+    symbol: "NFT",
     baseURI: "https://api.mocki.io/v1/67f898c5/test"
   };
-
 
   before(async () => {
     const contract = await ethers.getContractFactory("MyNFT");
@@ -26,24 +25,6 @@ describe("Non Fungible Token (NFT)", function() {
   it("Mint Non Fungible Token", async function() {
     await token.mint(accounts[0].address,attribute.baseURI);
     expect(await token.balanceOf(accounts[0].address)).to.equal("1");
-  });
-
-  it("Burn Non Fungible Token", async function() {
-    console.log("TODO");
-    // await token.burn();
-    // expect(await token.balanceOf(accounts[0].address)).to.equal("0");
-  });
-
-  it("Do not have permission to minting token", async function(){
-    console.log("TODO");
-    // const wallet = token.connect(accounts[2]);
-    // await expect(wallet.mint(accounts[2].address,amount)).to.be.reverted
-  });
-
-  it("Do not have permission to burning token", async function(){
-    console.log("TODO");
-    // const wallet = token.connect(accounts[2]);
-    // await expect(wallet.burn(accounts[2].address,amount)).to.be.reverted
   });
 
   it("Transfer Token", async function(){
@@ -64,6 +45,26 @@ describe("Non Fungible Token (NFT)", function() {
 
   it("Can not transfer to empty account", async () => {
     await expect(token.transferFrom(accounts[0].address, zero_address, "1")).to.be.reverted;
+  });
+
+  //#################### TODO test-case ####################
+
+  it("Burn Non Fungible Token", async function() {
+    console.log("TODO");
+    await token.burn(accounts[0].address,TokenId);
+    expect(await token.balanceOf(accounts[0].address)).to.equal("0");
+  });
+
+  it("Do not have permission to minting token", async function(){
+    console.log("TODO");
+    const wallet = token.connect(accounts[i]);
+    await expect(wallet.mint(accounts[i].address,TokenId)).to.be.reverted
+  });
+
+  it("Do not have permission to burning token", async function(){
+    console.log("TODO");
+    const wallet = token.connect(accounts[i]);
+    await expect(wallet.burn(accounts[i].address,TokenId)).to.be.reverted
   });
   
 });
